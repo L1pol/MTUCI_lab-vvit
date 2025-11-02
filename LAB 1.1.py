1ae02d2931a69fd5d2cce729f1a126a1680b1f09
L1pol <145706298+L1pol@users.noreply.github.com>
2025-11-02 17:30:12 +0300

    Add files via upload

---
+++ LAB 1.1.py
@@ -0,0 +1,14 @@
+'''
+#Задача 1
+num = int(input('Введине число'))
+for a in range(1,num+1):
+    print(a)
+'''
+
+
+num1 = int(input('Введите первое число:'))
+num2 = int(input('Введите второе число:'))
+if num1 > num2:
+    print('Большее число:', num1)
+else:
+    print('Большее число:', num2)
